b20728f1677a473f7e7cf2daedd12bd3f28a6d39
weltoncerqueira <143881974+weltoncerqueira@users.noreply.github.com>
2025-03-29 18:58:57 -0300

    Update AvaliaçãoClass.java

--- AvaliaçãoClass.java
+++ AvaliaçãoClass.java
@@ -1,47 +1,70 @@
 class AvaliaLivroFilmeSerie {
     private final CadastraLivroFilmeSerie listcadastro;
-    // Scanner scanner = new Scanner(System.in);
 
     public AvaliaLivroFilmeSerie(CadastraLivroFilmeSerie cadastro) {
         this. listcadastro = cadastro;
     }
 
-    public void AvaliaLivro() {
+    public void AvaliaLivro(int avaliacaoUsuario, String tituloOriginal) {
         List<Livro> listAvaliaLivro =  listcadastro.getListLivro();
         if (listAvaliaLivro.isEmpty()) {
             System.out.println("Nenhum livro cadastrado para avaliação.");
             return;
         }
-        for (Livro item : listAvaliaLivro) {
-            System.out.print("Avalie o livro (de 1 a 5): ");
-            int avaliacao = obterAvaliacao();
-            item.setAvaliacao(avaliacao);
-            System.out.println("Livro: " + item);
+        for (Livro livro : listAvaliaLivro) {
+            if (!livro.getTitulo().equals(tituloOriginal))
+                System.out.println("Livro não encontrado no banco de dados");
+            else {
+                livro.setAvaliacao(avaliacaoUsuario);
+                System.out.println("Livro: " + livro);
+            }
         }
     }
 
-    public void AvaliaFilme() {
+    public void AvaliaFilme(int avaliacaoUsuario, String tituloOriginal) {
         List<Filme> listAvaliaFilme =  listcadastro.getListFilme();
         if (listAvaliaFilme.isEmpty()) {
-            System.out.print("Nenhum livro cadastrado para avaliação.");
+            System.out.print("Nenhum Filme cadastrado para avaliação.");
             return;
         }
-        for (Filme item : listAvaliaFilme) {
-            System.out.print("Avalie o livro (de 1 a 5): ");
-            int avaliacao = obterAvaliacao();
-            item.setAvalia_filme(avaliacao);
-            System.out.println("Livro: " + item);
+        for (Filme filme : listAvaliaFilme) {
+            if (!filme.getTitulo().equals(tituloOriginal))
+                System.out.println("Filme não encontrado no banco de dados");
+            else {
+                filme.setAvalia_filme(avaliacaoUsuario);
+                System.out.println("Livro: " + filme);
+            }
         }
     }
 
-    private int obterAvaliacao() {
-        System.out.print("Avalie (de 1 a 5): ");
-        int avaliacao = 5;
+    public void AvaliaSerie(int avaliacaoUsuario, String tituloOriginal){
+        List<Serie> listAvaliaTemporada = listcadastro.getListSerie();
 
-        while (avaliacao < 1 || avaliacao > 5) {
-            System.out.print("Avaliação inválida. Insira um valor de 1 a 5: ");
-            avaliacao = 3;
+        if (listAvaliaTemporada.isEmpty()) {
+            System.out.println("Nenhuma série cadastrada para avaliação.");
+            return;}
+        for (Serie serie : listAvaliaTemporada){
+            if(!serie.getTitulo().equals(tituloOriginal))
+                System.out.println("Série não encontrada no banco de dados");
+            else{
+                serie.getTemporada().getFirst().setPontuacao(avaliacaoUsuario);
+                System.out.println("AvaliaSerie: " + serie.getTemporada().getFirst().getPontuacao());
+            }
         }
-        return avaliacao;
+    }
+
+    public void TemporadasReview(String review, String tituloOriginal){
+        List<Serie> listAvaliaTemporada = listcadastro.getListSerie();
+        if (listAvaliaTemporada.isEmpty()) {
+            System.out.println("Nenhuma série cadastrada para avaliação.");
+            return;}
+        for (Serie serie : listAvaliaTemporada){
+            if(!serie.getTitulo().equals(tituloOriginal)){
+                System.out.println("Série não encontrada no banco de dados");
+            } else{
+                serie.getTemporada().getFirst().setReview(review);
+                System.out.println("Review: " + serie.getTemporada().getFirst().getReview());
+            }
+        }
     }
 }
