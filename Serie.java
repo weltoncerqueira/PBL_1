43148a3a42d39f16ee44f100c913ca3e4e7fc401
weltoncerqueira <143881974+weltoncerqueira@users.noreply.github.com>
2025-03-29 11:38:39 -0300

    Update SerieTemporada.java

--- Serie.java
+++ Serie.java
@@ -1,65 +1,74 @@
-public class Serie {
+class Serie {
     private String titulo;
     private String gender;
-    private String anolancamento;
-    private String anoEncerramento;
+    private Year anoLancamento;
+    private Year anoEncerramento;
     private String elenco;
     private String tituloOriginal;
     private String ondeVer;
 
-    public String getElenco() {
-        return elenco;
-    }
-
-    public String getTituloOriginal() {
-        return tituloOriginal;
-    }
-
-    public String getAnolancamento() {
-        return anolancamento;
-    }
-
-    public void setAnolancamento(String anolancamento) {
-        this.anolancamento = anolancamento;
-    }
-
-    public String getAnoEncerramento() {
-        return anoEncerramento;
-    }
-
-    public void setAnoEncerramento(String anoEncerramento) {
+    public Serie(String titulo, String gender, Year anoLancamento, Year anoEncerramento, String elenco, String tituloOriginal, String ondeVer) {
+        this.titulo = titulo;
+        this.gender = gender;
+        this.anoLancamento = anoLancamento;
         this.anoEncerramento = anoEncerramento;
-    }
-
-    public String getOndeVer() {
-        return ondeVer;
-    }
-
-    public void setOndeVer(String ondeVer) {
+        this.elenco = elenco;
+        this.tituloOriginal = tituloOriginal;
         this.ondeVer = ondeVer;
     }
+    public String getElenco() {return elenco;}
+    public String getTituloOriginal() {return tituloOriginal;}
+    public Year getAnoLancamento() {return anoLancamento;}
+    public void setAnoLancamento(Year anoLancamento) {this.anoLancamento = anoLancamento;}
+    public Year getAnoEncerramento() {return anoEncerramento;}
+    public void setAnoEncerramento(Year anoEncerramento) {this.anoEncerramento = anoEncerramento;}
+    public String getOndeVer() {return ondeVer;}
+    public void setOndeVer(String ondeVer) {this.ondeVer = ondeVer;}
+    public void setTituloOriginal(String tituloOriginal) {this.tituloOriginal = tituloOriginal;}
+    public void setElenco(String elenco) {this.elenco = elenco;}
+    public String getTitulo() {return titulo;}
+    public void setTitulo(String titulo) {this.titulo = titulo;}
+    public String getGender() {return gender;}
+    public void setGender(String gender) {this.gender = gender;}
+    @Override
+    public String toString() {
+        return "{" +
+                "titulo='" + titulo + '\'' +
+                ", genero='" + gender + '\'' +
+                ", anoLancamento='" + anoLancamento + '\'' +
+                ", anoEncerramento='" + anoEncerramento + '\'' +
+                ", elenco='" + elenco + '\'' +
+                ", tituloOriginal='" + tituloOriginal + '\'' +
+                ", ondeVer='" + ondeVer + '\'' +
+                '}';}
+}
 
-    public void setTituloOriginal(String tituloOriginal) {
-        this.tituloOriginal = tituloOriginal;
+
+class serieTemporadas{
+    private String ano;
+    private String qtdEpisodes;
+
+    public serieTemporadas(String ano, String qtdEpisodes){
+        this.ano = ano;
+        this.qtdEpisodes = qtdEpisodes;
     }
-
-    public void setElenco(String elenco) {
-        this.elenco = elenco;
+    public String getAno() {
+        return ano;
     }
-
-    public String getTitulo() {
-        return titulo;
+    public void setAno(String ano) {
+        this.ano = ano;
     }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public String getQtdEpisodes() {
+        return qtdEpisodes;
     }
-
-    public String getGender() {
-        return gender;
+    public void setQtdEpisodes(String qtdEpisodes) {
+        this.qtdEpisodes = qtdEpisodes;
     }
-
-    public void setGender(String gender) {
-        this.gender = gender;
+    @Override
+    public String toString() {
+        return "{" +
+                "ano: " + ano + '\'' +
+                ", qtdEpisodes: " + qtdEpisodes + '\'' +
+                '}';
     }
 }
